086ddccd2c1522db47d03bffa8af48f63160e32d
Samuel <s.mayer@students.htl-leonding.ac.at>
2025-04-08 15:44:28 +0200

    ADD[backend]: adding new methode to find last station a whale was near

--- code/whale/src/main/java/at/ac/htlleonding/control/WhaleTrackerRepository.java
+++ code/whale/src/main/java/at/ac/htlleonding/control/WhaleTrackerRepository.java
@@ -1,15 +1,15 @@
 package at.ac.htlleonding.control;
 
 import at.ac.htlleonding.model.ResearchSite;
-import at.ac.htlleonding.model.ResearchStation;
 import at.ac.htlleonding.model.Whale;
 import at.ac.htlleonding.model.WhaleTrackerHistory;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.TypedQuery;
 
 import java.sql.Timestamp;
 import java.time.Instant;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -17,6 +17,9 @@ import java.util.List;
 public class WhaleTrackerRepository implements PanacheRepository<WhaleTrackerHistory> {
 
     public static final int EARTH_RADIUS = 6371;
+
+    @Inject
+    WhaleRepository whaleRepository;
 
     public List<WhaleTrackerHistory> getTrackerHistory(Whale whale) {
         return find("whale", whale).list();
@@ -26,9 +29,9 @@ public class WhaleTrackerRepository implements PanacheRepository<WhaleTrackerHis
         int distance = 0;
 
         for (WhaleTrackerHistory history : getTrackerHistory(whale)) {
-            if (history.getTrackerId().getTimestamp().after(Timestamp.from(Instant.from(from))) &&
-                    history.getTrackerId().getTimestamp().before(Timestamp.from(Instant.from(to)))) {
-                distance += calculateDistance(history.getTrackerId().getLat(), history.getTrackerId().getLon(), history.getTrackerId().getLat(), history.getTrackerId().getLon());
+            if (history.getTimestamp().after(Timestamp.from(Instant.from(from))) &&
+                    history.getTimestamp().before(Timestamp.from(Instant.from(to)))) {
+                distance += calculateDistance(history.getPosition().getLat(), history.getPosition().getLon(), history.getPosition().getLat(), history.getPosition().getLon());
             }
         }
 
@@ -49,6 +52,28 @@ public class WhaleTrackerRepository implements PanacheRepository<WhaleTrackerHis
     }
 
     public ResearchSite getLastVisitedSite(Whale whale) { //Checks when the position of a whale last matched a research station's one
-        return null;
+        final int RANGE = 10;
+
+        TypedQuery<ResearchSite> typedQuery = this.getEntityManager().createQuery(
+                "select r from ResearchSite r " +
+                        "join WhaleTrackerHistory wH " +
+                        "on wh.whale.id = :whaleId " +
+                        "where SQRT( " +
+                        "  POWER( " +
+                        "    RADIANS(wh.position.lat) - RADIANS(r.position.lat) " +
+                        "  ) + " +
+                        "  POWER( " +
+                        "    (RADIANS(wh.position.lon) - RADIANS(r.position.lon)) * COS(RADIANS((wh.position.lat + r.position.lat) / 2)) " +
+                        "  ) " +
+                        ") * :earthRadius < :range " +
+                        "order by wh.timestamp desc "
+                , ResearchSite.class
+        );
+
+        typedQuery.setParameter("whaleId", whale.getId());
+        typedQuery.setParameter("earthRadius", EARTH_RADIUS);
+        typedQuery.setParameter("range", RANGE);
+
+        return typedQuery.getSingleResult();
     }
 }
